79710c6c3d314a20e507b40c26187489ee521fd0
John De Leon <john.deleon@simplyrfid.com>
2022-11-18 15:54:39 +0800

    Updates

---
+++ precache-manifest.371ae8866e8640811ce5f1b963318988.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a868e8cbb055421ed2c6ae7a807166",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "2e13197fb8ce05d4ad52",
+    "url": "/react-tic-tac-toe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "88b6284567d7b5b01efc",
+    "url": "/react-tic-tac-toe/static/js/2.e99b4167.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tic-tac-toe/static/js/2.e99b4167.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2e13197fb8ce05d4ad52",
+    "url": "/react-tic-tac-toe/static/js/main.f9f7247e.chunk.js"
+  },
+  {
+    "revision": "9aec7fd96a8bb22d4f01",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.a7389be4.js"
+  }
+]);
